2fe0e361f08362616cb7c81f17324ef0db16fb47
dave <dave@163.com>
2021-07-12 00:27:48 +0800

    init project

---
+++ future/src/main/java/com/guru/future/FutureApplication.java
@@ -0,0 +1,13 @@
+package com.guru.future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureApplication.class, args);
+	}
+
+}
---
+++ future/src/test/java/com/guru/future/FutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.future;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
