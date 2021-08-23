9d1ea6b6dfbc3a5a25b4cca0890fedbc722557aa
dave <dave>
2021-08-23 12:15:03 +0800

    update daily

--- future/src/main/java/com/guru/future/biz/service/FutureMonitorService.java
+++ future/src/main/java/com/guru/future/biz/service/FutureMonitorService.java
@@ -25,8 +25,8 @@ public class FutureMonitorService {
     private static List<Pair<Integer, Float>> MONITOR_PARAMS = new ArrayList<>();
 
     static {
-        MONITOR_PARAMS.add(Pair.with(15, 0.2F));
-        MONITOR_PARAMS.add(Pair.with(30, 0.33F));
+        MONITOR_PARAMS.add(Pair.with(16, 0.2F));
+        MONITOR_PARAMS.add(Pair.with(32, 0.33F));
         MONITOR_PARAMS.add(Pair.with(50, 0.46F));
     }
 
